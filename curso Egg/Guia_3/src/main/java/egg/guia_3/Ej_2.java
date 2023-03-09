35a1f255bdad8026e484e12b31f1ebf75937cbbe
“Martin <jackmarinome@gmail.com>
2023-03-09 11:30:34 -0300

    primero

---
+++ curso Egg/Guia_3/src/main/java/egg/guia_3/Ej_2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package egg.guia_3;
+
+/**
+ *
+ * @author user
+ */
+public class Ej_2{ 
+        public static void main(String[] args){
+            
+        }
+
+}
---
+++ curso Egg/Guia_3/src/main/java/egg/guia_3/Guia_3.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package egg.guia_3;
+
+/**
+ *
+ * @author user
+ */
+public class Guia_3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
